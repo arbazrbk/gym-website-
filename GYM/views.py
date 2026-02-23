5e8ff914a7716badb300f4185cced9fb1437b876
Arbaz Khan <arbazkhanrbk@gmail.com>
2026-02-23 23:23:05 +0500

    makes some changes

--- GYM/views.py
+++ GYM/views.py
@@ -10,12 +10,38 @@ def about(request):
     return render(request, 'GYM/about.html')
 
 def programs(request):
-    user = request.user
-    program_id = request.GET.get('program_id')
-    return render(request, 'GYM/programs.html', {'program_id': program_id, 'user': user})
+    return render(request, 'GYM/programs.html')
 
 def program_single(request):
-    return render(request, 'GYM/program-single.html')
+    user = request.user
+    program_id = request.GET.get('program_id')
+    weight  = request.GET.get('age')
+    height = request.GET.get('height')
+    bmi = 0.0
+    if weight and height:
+        try:
+            weight = float(weight)
+            height = float(height) / 100  
+            bmi = weight / (height ** 2)
+            if bmi < 18.5:
+                messages.info(request, 'Your BMI is {:.2f}. You are underweight.'.format(bmi))
+            elif 18.5 <= bmi < 25:
+                messages.info(request, 'Your BMI is {:.2f}. You have a normal weight.'.format(bmi))
+            elif 25 <= bmi < 30:
+                messages.info(request, 'Your BMI is {:.2f}. You are overweight.'.format(bmi))
+            else:
+                messages.info(request, 'Your BMI is {:.2f}. You are obese.'.format(bmi))            
+        except ValueError:
+            messages.error(request, 'Invalid input for weight or height. Please enter numeric values.')
+    else:
+        messages.error(request, 'Please provide both weight and height to calculate BMI.')
+    
+    return render(request, 'GYM/programs.html', {'program_id': program_id, 'user': user, 'bmi': bmi} )
+
+
+def health_plan(request):
+    weight_loss = 
+    
 
 def classes(request):
     return render(request, 'GYM/classes.html')
